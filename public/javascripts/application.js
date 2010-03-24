004ebb61d4598b149e98e9465bb481b0a5eba5ea
Stephen Jarman <steve@sailing-fast.com>
2010-03-24 00:11:07 +0000

    nicer interface implemented with basic jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  
+  $("#new-post a.show").click(function() {
+    $(this).slideUp(200);
+    $("#new-post form").delay(200).slideDown(500);
+    return false;
+  });
+  
+  $("#new-post a.hide").click(function() {
+    $("#new-post form").slideUp(500);
+    $("#new-post a.show").delay(500).slideDown(200);
+    return false;
+  });
+  
+});
