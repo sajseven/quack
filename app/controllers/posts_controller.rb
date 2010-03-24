7f833c2d5860861f8ea1ac542b904dd534e87613
Stephen Jarman <steve@sailing-fast.com>
2010-03-24 23:00:24 +0000

    edit functionality implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,29 @@ class PostsController < ApplicationController
     @post = Post.find params[:id]
   end
   
+  def edit
+    @post = Post.find params[:id]
+  end
+  
   def create
     @post = Post.new params[:post]
     if @post.save
-      flash[:notice] = "Post was successfully saved."
+      flash[:notice] = "Post was successfully created."
       redirect_to @post
     else
-      flash[:notice] = "Post could not be saved."
+      flash[:notice] = "Post could not be created."
       render :action => "index"
+    end
+  end
+  
+  def update
+    @post = Post.find params[:id]
+    if @post.update_attributes params[:post]
+      flash[:notice] = "Post was successfully updated."
+      redirect_to @post
+    else
+      flash[:notice] = "Post could not be updated."
+      render edit_post_path @post
     end
   end
 end
